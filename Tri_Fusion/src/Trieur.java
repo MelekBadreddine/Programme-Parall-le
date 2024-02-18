8dcd08a16dbf653d7c146ecc24fbfe373f3ff50a
Melek Badreddine <119127830+MelekBadreddine@users.noreply.github.com>
2024-02-18 14:32:36 +0100

    Update Trieur.java

--- Tri_Fusion/src/Trieur.java
+++ Tri_Fusion/src/Trieur.java
@@ -1,12 +1,23 @@
-
-public class Trieur extends Thread{
-	int deb;
-	int fin;
-	public Trieur(int deb, int fin) {
-		this.deb=deb;
-		this.fin=fin;
-	}
-	public void run() {
+public class Trieur extends Thread {
+	int startIndex;
+	int endIndex;
+	Trieur(int startIndex,int endIndex){
+		this.startIndex=startIndex;
+		this.endIndex=endIndex;
 		
 	}
+	public void run() {
+        // Tri par insertion (Insertion Sort)
+        for (int i = startIndex + 1; i < endIndex; i++) {
+            int key = Principale.tab[i];
+            int j = i - 1;
+
+            while (j >= startIndex && Principale.tab[j] > key) {
+                Principale.tab[j + 1] = Principale.tab[j];
+                j = j - 1;
+            }
+
+            Principale.tab[j + 1] = key;
+        }
+    }
 }
