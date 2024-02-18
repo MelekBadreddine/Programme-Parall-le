a2f4ea214d5961f79b5ed52444fd1651eb7032f9
Melek Badreddine <119127830+MelekBadreddine@users.noreply.github.com>
2024-02-18 14:32:17 +0100

    Update Principale.java

--- Tri_Fusion/src/Principale.java
+++ Tri_Fusion/src/Principale.java
@@ -1,72 +1,62 @@
 import java.util.Scanner;
 
 public class Principale {
-	
-	static int n = 6;
-	//declaration du tableau
-	int[] tab = new int[n];
-	
-	void saisir() {
-		Scanner sc = new Scanner(System.in);
-		for( int i=0; i<n; i++) {
-			tab[i] = sc.nextInt();		
-		}
-	}
-	
-	void fusion() {
-		int i = 0;
-		int j = n/2;
-		int k = 0;
-		int[] w = new int[n];
-		while(i<n/2 && j<n) {
-			if(tab[i]<tab[j]) {
-				w[k] = tab[i];
-				i++;
-				k++;
-				if(i == n/2 - 1) {
-					for(int p=j; p<n; p++) {
-						w[k] = tab[p];
-						k++;
-					}
-				}
-				break;
-			}
-			else {
-				w[k] = tab[j];
-				j++;
-				k++;
-				if(j == n-1) {
-					for(int p=i; p<n/2; p++) {
-						w[k] = tab[p];
-						k++;
-					}
-				}
-				break;
-			}
-		}
-		tab = w;
-	}
-	
-	void affichage() {
-		for(int i=0; i<n; i++) {
-			System.out.println(tab[i]);
-		}
-	}
 
+	static int n=6;
+	//declartation du tab
+	static int[] tab = new int[n];
+	void saisi() {
+		Scanner Sc =new Scanner(System.in);
+		for (int i=0;i<n;i++) {
+			tab[i]=Sc.nextInt();
+		}
+		Sc.close();
+	}
+	void fusion() {
+		int[] w = new int[n];
+	    int i = 0, j = n / 2, k = 0;
+
+	    // Merging the two sorted arrays
+	    while (i < n / 2 && j < n) {
+	        if (tab[i] <= tab[j]) {
+	            w[k++] = tab[i++];
+	        } else {
+	            w[k++] = tab[j++];
+	        }
+	    }
+
+	    // Copy the remaining elements of the first half (if any)
+	    while (i < n / 2) {
+	        w[k++] = tab[i++];
+	    }
+
+	    // Copy the remaining elements of the second half (if any)
+	    while (j < n) {
+	        w[k++] = tab[j++];
+	    }
+
+	    // Update the original array with the sorted elements
+	    tab = w;
+	}
+	void affichage(){
+        for (int i = 0; i < n; i++) {
+            System.out.print(tab[i] + " ");
+        }
+		
+	}
 	public static void main(String[] args) {
-		Principale P = new Principale();
-		P.saisir();
-		Trieur t1 = new Trieur(0, n/2);
-		Trieur t2 = new Trieur(n/2, n);
+		Principale p=new Principale();
+		p.saisi();
+		Trieur t1=new Trieur(0,n/2);
+		Trieur t2=new Trieur(n/2,n);
 		t1.start();
 		t2.start();
 		try {
 			t1.join();
 			t2.join();
-			P.fusion();
-			P.affichage();
-		}
-		catch(InterruptedException e) {
+			p.fusion();
+			p.affichage();
+		}catch(InterruptedException e) {
 			
 		}
 
